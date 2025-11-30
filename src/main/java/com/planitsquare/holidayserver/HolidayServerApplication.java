9bb3661d7fe675306aa1ba6126e9bc79ffb238ae
SON1205 <jjinoson1205@gmail.com>
2025-11-30 14:16:20 +0900

    init

---
+++ src/main/java/com/planitsquare/holidayserver/HolidayServerApplication.java
@@ -0,0 +1,13 @@
+package com.planitsquare.holidayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayServerApplication.class, args);
+	}
+
+}
